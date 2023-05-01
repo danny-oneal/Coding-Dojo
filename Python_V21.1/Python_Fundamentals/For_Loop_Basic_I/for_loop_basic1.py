ec597d49719946f911c1abd9a43f4061925ca251
danny-oneal <danny.e.oneal@gmail.com>
2023-04-30 23:20:16 -0700

    completed

---
+++ Python_V21.1/Python_Fundamentals/For_Loop_Basic_I/for_loop_basic1.py
@@ -0,0 +1,34 @@
+# Basic
+for x in range(0, 151):
+    print(x)
+
+# Multiples of Five
+for x in range(5, 1001, 5):
+    print(x)
+
+# Counting, the Dojo Way
+for x in range(1, 101):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+    else:
+        print(x)
+
+# Whoa. That Sucker's Huge
+sum = 0
+for x in range(1, 500000, 2):
+    sum += x
+print(sum)
+
+# Countdown by Fours
+for x in range(2018, -1, -4):
+    print(x)
+
+# Flexible Counter
+lowNum = 2
+highNum = 9
+mult = 3
+for x in range(lowNum, highNum + 1):
+    if x % mult == 0:
+        print(x)
