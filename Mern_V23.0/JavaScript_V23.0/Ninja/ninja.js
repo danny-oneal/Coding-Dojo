871a4af39fb743d5f30f0a4e0e6e9327c4c45cc5
danny-oneal <danny.e.oneal@gmail.com>
2023-06-02 18:52:35 -0700

    mern: ninja

---
+++ Mern_V23.0/JavaScript_V23.0/Ninja/ninja.js
@@ -0,0 +1,25 @@
+class Ninja {
+    constructor(name, health = 90, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName() {
+        console.log(`Name: ${this.name}`);
+    }
+
+    showStats() {
+        console.log(`
+        Name: ${this.name}
+        Health: ${this.health}
+        Speed: ${this.speed}
+        Strength: ${this.strength}
+        `);
+    }
+
+    drinkSake() {
+        this.health += 10;
+    }
+}
