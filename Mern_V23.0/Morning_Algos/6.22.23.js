8227f1c5f684ffadb294bbac045d1c1a73b65c33
danny-oneal <danny.e.oneal@gmail.com>
2023-06-22 10:23:32 -0700

    flattenObjectOfArrays

---
+++ Mern_V23.0/Morning_Algos/6.22.23.js
@@ -0,0 +1,56 @@
+const object1 = {
+    closedCreditMemos: [],
+    closedDeliveryOrders: [],
+    closedPickupOrders: [
+        { id: 112, type: "pickup" },
+        { id: 117, type: "pickup" }
+    ],
+    openCreditMemos: [],
+    openDeliveryOrders: [
+        {
+            id: 123,
+            type: "delivery",
+            gateCode: "#2552"
+        },
+        {
+            id: 153,
+            type: "delivery",
+            instructions: "Place in secure delivery box."
+        }
+    ],
+    openPickupOrders: [
+        {
+            id: 123,
+            type: "pickup"
+        }
+    ],
+    returnPickupOrders: []
+};
+
+const expected1 = [
+    { id: 112, type: "pickup" },
+    { id: 117, type: "pickup" },
+    { id: 123, type: "delivery" },
+    { id: 153, type: "delivery" },
+    { id: 123, type: "pickup" }
+];
+
+/**
+ * Takes an object containing arrays of objects and places all the nested
+ * objects into a new one-dim array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Object} o Containing arrays of objects.
+ * @returns {Object[]} An array of objects.
+ */
+function flattenObjectOfArrays(orderObj) {
+    const result = [];
+    for (const orders in orderObj) {
+        for (const { id, type } of orderObj[orders]) {
+            result.push({ id, type });
+        }
+    }
+    return result;
+}
+
+console.log(flattenObjectOfArrays(object1), expected1);
